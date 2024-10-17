5185ad1dc5c50e9870ad1ec21484630c7c5807f2
Ayormitan <kolawoleabaz97@gmail.com>
2024-10-17 04:36:23 -0700

    Add country ENU to database

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- Create new user with attributes expecially country
-CREATE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS user (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
